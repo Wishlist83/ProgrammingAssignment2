7e6d55225c1b9e7b49d3ccbb95a92ed1fa2e707f
Wishlist83 <markkaptein@gmail.com>
2017-01-13 11:44:27 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(c(1,0,0,1), nrow=2, ncol=2)) {
+        #Functie om  de inverse van een matrix te cachen
+        
+        # Bij de eerste keer aanroepen maken we een lege variabele voor de 
+        # inverse
+        inv <- NULL
+        # Als we set aanroepen vullen we de matrix via set-functie
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        # De get functie geeft de waarde x terug
+        get <- function() x
+        # Dan de functie om de inverse te setten
+        setinv <- function(solve) inv <<- solve
+        # En op te halen
+        getinv <- function() inv
+        # Tot slot geven we een lijst van functies terug die we op de matrix
+        # en zijn inverse kunnen aanroepen
+        list(set = set, get = get, setinv = setinv, getinv=getinv)        
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # Functie om de gecachete inverse van een matrix op te halen
+        
+        # Probeer eerst de inverse op te halen
+        inv <- x$getinv()
+        # Als deze al bestaat, zijn we klaar en geven we de inverse terug
+        if(!is.null(inv)) {
+                message("Getting cached inverse")
+                return(inv)
+        }
+        # In andere gevallen gaan we de inverse uitrekenen en opslaan voor
+        # toekomstig gebruik
+        # Haal de matrix op
+        data <- x$get()
+        # Bereken de inverse
+        inv <- solve(data, ...)
+        # Sla het resultaat op in de cachematrix
+        x$setinv(inv)
+        # Toon de inverse als output
+        inv
 }
